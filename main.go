c0da56d7875e4ab6c50f1d41607b306c6f5611d5
Daniel Fain <d6niel@protonmail.com>
2019-06-30 10:56:03 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/anacrolix/torrent"
+)
+
+func main() {
+	config := initClientConfig()
+	client, _ := torrent.NewClient(config)
+	defer client.Close()
+
+	infoHash := fromInfoHashString("8543AC1F905954E0EC0F8E487646601C9F9F41CF")
+	torrent, _ := client.AddTorrentInfoHash(infoHash)
+
+	<-torrent.GotInfo()
+	torrent.DownloadAll()
+	client.WaitAll()
+	log.Print("torrent finished downloading")
+}
+
+func initClientConfig() *torrent.ClientConfig {
+	home, _ := os.UserHomeDir()
+	config := torrent.NewDefaultClientConfig()
+	config.DataDir = home + "\\Downloads"
+	return config
+}
+
+func fromInfoHashString(hexString string) torrent.InfoHash {
+	var infoHash torrent.InfoHash
+	infoHash.FromHexString(hexString)
+	return infoHash
+}
