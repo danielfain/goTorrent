bb14324f82c3f6cc9a7e80a260a2d5fbf33e3997
Daniel Fain <d6niel@protonmail.com>
2019-07-14 00:53:49 -0400

    cobra download command work

--- cmd/download.go
+++ cmd/download.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/anacrolix/torrent"
 	"github.com/spf13/cobra"
 )
 
@@ -10,11 +12,41 @@ import (
 var downloadCmd = &cobra.Command{
 	Use:   "download",
 	Short: "Download a torrent from a magnet or infohash",
-	Long:  "Download a torrent from a magnet or infohash",
 	Run: func(cmd *cobra.Command, args []string) {
-		t, _ := cmd.Flags().GetString("type")
-		fmt.Println(t)
+		config := initClientConfig()
+		client, _ := torrent.NewClient(config)
+		defer client.Close()
+
+		torrentType, _ := cmd.Flags().GetString("type")
+
+		if torrentType == "infohash" || torrentType == "hash" {
+			infoHash := fromInfoHashString(args[0])
+			tor, _ := client.AddTorrentInfoHash(infoHash)
+			<-tor.GotInfo()
+			tor.DownloadAll()
+			if client.WaitAll() == true {
+				fmt.Println("torrent successfully downloaded")
+			}
+			fmt.Println("there was an error while downloading")
+		}
+
+		if torrentType == "magnet" {
+
+		}
 	},
+}
+
+func initClientConfig() *torrent.ClientConfig {
+	home, _ := os.UserHomeDir()
+	config := torrent.NewDefaultClientConfig()
+	config.DataDir = home + "\\Downloads"
+	return config
+}
+
+func fromInfoHashString(hexString string) torrent.InfoHash {
+	var infoHash torrent.InfoHash
+	infoHash.FromHexString(hexString)
+	return infoHash
 }
 
 func init() {
--- main.go
+++ main.go
@@ -1,38 +1,9 @@
 package main
 
 import (
-	"os"
-
-	"github.com/anacrolix/torrent"
 	"github.com/danielfain/goTorrent/cmd"
 )
 
 func main() {
 	cmd.Execute()
-	/*
-		config := initClientConfig()
-		client, _ := torrent.NewClient(config)
-		defer client.Close()
-
-		infoHash := fromInfoHashString("8543AC1F905954E0EC0F8E487646601C9F9F41CF")
-		torrent, _ := client.AddTorrentInfoHash(infoHash)
-
-		<-torrent.GotInfo()
-		torrent.DownloadAll()
-		client.WaitAll()
-		log.Print("torrent finished downloading")
-	*/
-}
-
-func initClientConfig() *torrent.ClientConfig {
-	home, _ := os.UserHomeDir()
-	config := torrent.NewDefaultClientConfig()
-	config.DataDir = home + "\\Downloads"
-	return config
-}
-
-func fromInfoHashString(hexString string) torrent.InfoHash {
-	var infoHash torrent.InfoHash
-	infoHash.FromHexString(hexString)
-	return infoHash
 }
